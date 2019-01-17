cea1efb83bf75de020c80a65b6081c26a58576a6
Tom Marsh <ts.marsh@gmail.com>
2019-01-17 02:25:52 +0000

    Adding disconnect

---
+++ api/disconnect/src/connect/core.clj
@@ -0,0 +1,41 @@
+(ns disconnect.core
+  (:require [clojure.data.json :as json])
+  (:gen-class
+   :methods [^:static [handler [java.util.Map] String]]))
+
+
+(defprotocol ConvertibleToClojure
+  (->cljmap [o]))
+
+(extend-protocol ConvertibleToClojure
+  java.util.Map
+  (->cljmap [o] (let [entries (.entrySet o)]
+                (reduce (fn [m [^String k v]]
+                          (assoc m (keyword k) (->cljmap v)))
+                        {} entries)))
+
+  java.util.List
+  (->cljmap [o] (vec (map ->cljmap o)))
+
+  java.lang.Object
+  (->cljmap [o] o)
+
+  nil
+  (->cljmap [_] nil))
+
+(def response-template {
+                        :isBase64Encoded false
+                        :statusCode 200
+                        :headers {}
+                        :body {:json "OK"}
+                        })
+
+
+(defn -handler [s]
+  (let [event (->cljmap s)]
+    (println (str "event: " event))
+    (println (str "Response" (json/write-str response-template)))
+    (println (str "ConnectionId:" (-> event
+                                   :requestContext
+                                   :connectionId)))
+    (json/write-str response-template)))
---
+++ api/disconnect/test/connect/core_test.clj
@@ -0,0 +1,7 @@
+(ns connect.core-test
+  (:require [clojure.test :refer :all]
+            [connect.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
