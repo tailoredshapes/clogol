4fa4b3b53699dceef9935fe82807692e6762f59d
Tom Marsh <ts.marsh@gmail.com>
2019-01-19 13:05:42 +0000

    Removing boilerplate

--- api/live/src/live/core.clj
+++ api/live/src/live/core.clj
@@ -1,27 +1,9 @@
 (ns live.core
+  (:require [util.core :as util])
   (:gen-class
-   :methods [^:static [handler [java.util.Map] String]]))
+   :methods [^:static [handler [java.util.Map] java.util.Map]]))
 
-
-(defprotocol ConvertibleToClojure
-  (->cljmap [o]))
-
-(extend-protocol ConvertibleToClojure
-  java.util.Map
-  (->cljmap [o] (let [entries (.entrySet o)]
-                (reduce (fn [m [^String k v]]
-                          (assoc m (keyword k) (->cljmap v)))
-                        {} entries)))
-
-  java.util.List
-  (->cljmap [o] (vec (map ->cljmap o)))
-
-  java.lang.Object
-  (->cljmap [o] o)
-
-  nil
-  (->cljmap [_] nil))
 
 (defn -handler [s]
-  (println (->cljmap s))
-  (println "Hello World!"))
+  (let [event (util/->cljmap s)]
+    (println "Hello World!")))
